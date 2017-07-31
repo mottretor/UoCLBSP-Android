746ae5c9f7b5bb7a7a7b02a4825582fde33bc284
PasinduPriyashan <pasindu95.m@gmail.com>
2017-07-31 17:28:11 +0530

    Progress Update
    Date : 31/07/17

---
+++ UocLBSP-Source/app/src/main/java/com/group06/applications/uoclbsp_source/MainActivity.java
@@ -0,0 +1,25 @@
+package com.group06.applications.uoclbsp_source;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view){
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
+}
